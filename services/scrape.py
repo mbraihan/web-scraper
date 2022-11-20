f36bef6669d13e1b1e3af7cb64928fbeb82c9ec5
Mohammad Raihanul Bashar <rbfahim.iub13@outlook.com>
2022-11-21 02:08:28 +0600

    feat:scraper complete

--- services/scrape.py
+++ services/scrape.py
@@ -1,3 +1,4 @@
+import contextlib
 import requests
 from bs4 import BeautifulSoup
 from services.network import NetworkService
@@ -35,3 +36,76 @@ class DataScrapingService:
             data["last_modified_time"] = c7_data['content']
 
         return data or {}
+
+    def get_p_h_tag_data(self, soup):
+        available_tags = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p']
+
+        null_values = ['null', 'Null', 'NULL', 'none',
+                    'None', 'NONE', 'nil', 'Nil', 'NIL', '']
+        data = {tag: [tag_data.text.strip() for tag_data in soup.find_all(tag) if tag_data.text.strip() not in null_values] for tag in available_tags}
+
+        return data or {}
+
+    def get_links(self, soup, url):
+        data = {}
+        link_count = 1
+        url = url = f'https://{network_service.get_domain_name(url)}'
+        with contextlib.suppress(Exception):
+            for link in soup.find_all('a', href=True):
+                if '#' in link['href']:
+                    continue
+                elif '/' in link['href'][0]:
+                    c_url = url + link['href']
+                    variable = f'link-{str(link_count)}'
+                    link_count += 1
+                    data[variable] = str(c_url) + link['href']
+                else:
+                    variable = f'link-{str(link_count)}'
+                    link_count += 1
+                    data[variable] = link['href']
+        return data or {}
+
+    def get_image_data(self, soup, url):
+        data = {}
+        image_count = 1
+        url = f'https://{network_service.get_domain_name(url)}'
+        with contextlib.suppress(KeyError):
+            for link in soup.find_all('img'):
+                if '/' in link['src'][0]:
+                    if '/' in link['src'][1]:
+                        var2 = f'img-{str(image_count)}'
+                        data[var2] = 'https:'+link['src']
+                        image_count += 1
+
+                    if '/' not in link['src'][1]:
+                        var2 = f'img-{str(image_count)}'
+                        data[var2] = str(url) + link['src']
+                        image_count += 1
+                else:
+                    var2 = f'img-{str(image_count)}'
+                    data[var2] = link['src']
+                    image_count += 1
+        return data or {}
+
+    def get_data(self, url):
+        data = {}
+        try:
+            res = requests.get(url, timeout=5)
+            if res.status_code == 200:
+                bs4_soup = BeautifulSoup(res.text, 'lxml')
+                data['seo'] = self.get_seo_data(bs4_soup)
+                data['p_h_tags'] = self.get_p_h_tag_data(bs4_soup)
+                data['images'] = self.get_image_data(bs4_soup, url)
+                data['links'] = self.get_links(bs4_soup, url)
+                return data or {}
+            else:
+                return self.get_request_error_msg()
+        except requests.exceptions.RequestException:
+            return self.get_request_error_msg()
+
+    def get_request_error_msg(self):
+        return {
+            'status': 'error',
+            'message': 'Error in scraping the data',
+
+        }
