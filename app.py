54895fdf74ee2065f8b9990116ed66cabd50e71f
Mohammad Raihanul Bashar <rbfahim.iub13@outlook.com>
2022-11-21 00:50:38 +0600

    landing route

--- app.py
+++ app.py
@@ -7,3 +7,10 @@ from services.network import NetworkService
 app = Flask(__name__)
 
 url_validator = UrlValidators()
+
+@app.route('/', methods=['GET', 'POST'])
+def home_page():
+    if request.method != 'POST':
+        return render_template('index.html')
+    url = url_validator.clean_url(request.form.get('url'))
+    return redirect(f'/v1/scrape?url={url}') if url_validator.validate_url(url) else render_template('index.html', error='Invalid URL!')
