57b82400f037ba2fb2ad0ba591dbafe97a38be8e
Mohammad Raihanul Bashar <rbfahim.iub13@outlook.com>
2022-11-21 01:01:47 +0600

    feat:scraping

--- app.py
+++ app.py
@@ -14,3 +14,19 @@ def home_page():
         return render_template('index.html')
     url = url_validator.clean_url(request.form.get('url'))
     return redirect(f'/v1/scrape?url={url}') if url_validator.validate_url(url) else render_template('index.html', error='Invalid URL!')
+
+@app.route("/v1/scrape", methods=['GET'])
+def scrape_data():
+    url = url_validator.clean_url(request.args.get('url'))
+    if not (url_validator.validate_url(url)):
+        return jsonify({'error': 'Invalid URL!'}), 400
+
+    data_scraping_service = DataScrapingService()
+    data = data_scraping_service.get_data(url)
+
+    network_service = NetworkService()
+    data["network"] = network_service.get_data(url)
+
+    data["data_scraped_at"] = datetime.datetime.now()
+
+    return jsonify(data), 200
